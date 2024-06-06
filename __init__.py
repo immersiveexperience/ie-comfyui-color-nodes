41af831dc47e25a09c1c0bf85b86c9b992b855ec
till_greiffert <till.greiffert@mhp.com>
2024-06-06 16:15:32 +0200

    Added HexToColorNode to init.py

--- __init__.py
+++ __init__.py
@@ -3,4 +3,5 @@ from .nodes.nodes import *
 NODE_CLASS_MAPPINGS = {
     "Average Color": AverageColorNode,
     "Complementary Color": ComplementaryColorNode,
+    "Hex to Color Name": HexToColorNameNode,
 }
